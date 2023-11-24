5f3e32568d60b62243afa75d3c0a0db3f387187f
Aditya <codecommander03@gmail.com>
2023-11-24 22:44:01 +0530

    added keyboard navigations in login/signup

--- frontend/src/components/Authentication/Signup.js
+++ frontend/src/components/Authentication/Signup.js
@@ -1,9 +1,11 @@
-import React, { useState } from 'react'
-import { VStack } from '@chakra-ui/layout'
-import { FormControl, FormLabel } from '@chakra-ui/form-control'
-import { Input, InputGroup, InputRightElement } from '@chakra-ui/input'
-import { Button } from '@chakra-ui/button'
-import { useToast } from '@chakra-ui/react'
+import React, { useState,useRef } from 'react';
+import { VStack } from '@chakra-ui/layout';
+import { FormControl, FormLabel } from '@chakra-ui/form-control';
+import { Input, InputGroup, InputRightElement } from '@chakra-ui/input';
+import { Button } from '@chakra-ui/button';
+import { useToast } from '@chakra-ui/react';
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
 
 const Signup = () => {
     const [show, setShow] = useState(false);
@@ -14,6 +16,48 @@ const Signup = () => {
     const [pic, setPic] = useState();
     const [loading, setLoading] = useState(false);
     const toast = useToast();
+    const history = useHistory();
+
+    const nameRef = useRef(null);
+    const emailRef = useRef(null);
+    const passwordRef = useRef(null);
+    const confirmpasswordRef = useRef(null);
+
+    const keyName = (e) => {
+        if (e.key === 'ArrowDown') {
+            e.preventDefault();
+            setTimeout(() => {
+                emailRef.current.focus();
+            },2000)
+        }
+    }
+
+    const keyEmail = (e) => {
+        e.preventDefault();
+        if (e.key === 'ArrowDown') {
+            passwordRef.current.focus();
+        }
+        if (e.key === 'ArrowUp') {
+            nameRef.current.focus();
+        }
+    }
+
+    const keyPassword = (e) => {
+        e.preventDefault();
+        if (e.key === 'ArrowDown') {
+            confirmpasswordRef.current.focus();
+        }
+        if (e.key === 'ArrowUp') {
+            emailRef.current.focus();
+        }
+    }
+
+    const keyConfirmPassword = (e) => {
+        if (e.key === 'ArrowUp') {
+            e.preventDefault();
+            passwordRef.current.focus();
+        }
+    }
 
     const handleClick = () => setShow(!show);
     const postDetails = (pics) => {
@@ -28,6 +72,8 @@ const Signup = () => {
             });
             return;
         }
+        
+        console.log(pics);
 
         if (pics.type === 'image/jpeg' || pics.type === 'image/png') {
             const data = new FormData();
@@ -41,6 +87,7 @@ const Signup = () => {
             .then(res => res.json())
             .then(data => {
                 setPic(data.url.toString());
+                console.log(data.url.toString());
                 setLoading(false);
             })
             .catch(err => {
@@ -59,14 +106,69 @@ const Signup = () => {
             return;
         }
     }
-    const submitHandler = () => { }
+    const submitHandler = async () => {
+        setLoading(true);
+        if(!name || !email || !password || !confirmpassword){
+            toast({
+                title: "Please fill all fields",
+                status: "warning",
+                duration: 5000,
+                isClosable: true,
+                position: "top"
+            });
+            setLoading(false);
+            return;
+        }
+        if(password !== confirmpassword){
+            toast({
+                title: "Passwords do not match",
+                status: "warning",
+                duration: 5000,
+                isClosable: true,
+                position: "top"
+            });
+            return;
+        }
+
+        try {
+            const config = {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            }
+            const { data } = await axios.post('/api/user', { name, email, password, pic }, config);
+            toast({
+                title: "Registeration Successful",
+                status: "success",
+                duration: 5000,
+                isClosable: true,
+                position: "top"
+            });
+
+            localStorage.setItem('userInfo', JSON.stringify(data));
+            setLoading(false);
+            history.push('/chats');
+        } catch (error) {
+            toast({
+                title: "Something went wrong",
+                status: "error",
+                duration: 5000,
+                isClosable: true,
+                position: "top"
+            });
+            setLoading(false);
+        }
+    };
 
     return <VStack spacing='5px'>
         <FormControl id='first-name' isRequired>
             <FormLabel>Name</FormLabel>
             <Input
                 placeholder="Enter Your Name"
-                onChange={(e) => setName(e.target.value)}>
+                onChange={(e) => setName(e.target.value)}
+                ref={nameRef}
+                onKeyDown={keyName}
+            >
             </Input>
         </FormControl>
 
@@ -74,7 +176,11 @@ const Signup = () => {
             <FormLabel>Email</FormLabel>
             <Input
                 placeholder="Enter Your Email"
-                onChange={(e) => setEmail(e.target.value)}>
+                onChange={(e) => setEmail(e.target.value)}
+                ref={emailRef}
+                onKeyDown={keyEmail}
+                onKeyUp={keyEmail}
+            >
                 </Input>
         </FormControl>
 
@@ -85,6 +191,9 @@ const Signup = () => {
                     type={show ? 'text' : 'password'}
                     placeholder="Enter Your Password"
                     onChange={(e) => setPassword(e.target.value)}
+                    ref={passwordRef}
+                    onKeyUp={keyPassword}
+                    onKeyDown={keyPassword}
                 />
                 <InputRightElement width="4.5rem">
                     <Button h="1.75rem" size="sm" onClick={handleClick}>
@@ -101,6 +210,8 @@ const Signup = () => {
                     type={show ? 'text' : 'password'}
                     placeholder="Confirm Password"
                     onChange={(e) => setConfirmpassword(e.target.value)}
+                    ref={confirmpasswordRef}
+                    onKeyUp={keyConfirmPassword}
                 />
                 <InputRightElement width="4.5rem">
                     <Button h="1.75rem" size="sm" onClick={handleClick}>
@@ -124,10 +235,12 @@ const Signup = () => {
             colorScheme='green'
             width='100%'
             style={{ marginTop: 15 }}
-        onClick={submitHandler}>
+            onClick={submitHandler}
+            isLoading={loading}
+        >
             Sign Up
         </Button>
     </VStack>
 }
 
-export default Signup
+export default Signup 
